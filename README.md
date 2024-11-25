bf894822b7acb00763488c92a00f926647be76e1
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-25 21:11:53 +0530

    trained model and predict output by training yolo11n and yolo5n

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## ML Football Analysis
+
+Dataset link: https://universe.roboflow.com/roboflow-jvuqo/football-players-detection-3zvbc/dataset/1
--- yolo_inference.py
+++ yolo_inference.py
@@ -1,3 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
+# To predict the output from the trained model
+model = YOLO('models/')
+
+results = model.predict('input_videos/08fd33_4.mp4', save=True)
+print(results[0])
+for box in results[0].boxes:
+    print(box)
