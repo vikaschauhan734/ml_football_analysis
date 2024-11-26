a414627e2f487c5338b04b593bd49398476e7e99
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-26 20:17:20 +0530

    trained model and predict output by training yolo5l and yolo11l

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # To predict the output from the trained model
-model = YOLO('models/')
+model = YOLO('models/best_11l.pt')
 
 results = model.predict('input_videos/08fd33_4.mp4', save=True)
 print(results[0])
