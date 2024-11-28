babd0bbb043e6f674f406bb9aa799e07aa8b6f02
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-28 19:39:00 +0530

    inside tracker bounding box predicted successfully

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from src.utils.video_utils import read_video, save_video
+from src.trackers.tracker import tracker
 
 def main():
     # Read video
     video_frames = read_video('input_videos/08fd33_4.mp4')
+
+    #Initialize tracker
+    tracke = tracker('models/best_11l.pt')
+
+    tracks = tracke.get_object_tracks(video_frames)
 
     # Save video
     save_video(video_frames, 'output_video/output_video.avi')
