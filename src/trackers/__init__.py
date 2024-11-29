e2696b6d14e690864a7c5dc28dd20dd368c8b7bc
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-29 21:40:43 +0530

    converted goalkeeper class to player class in tracker and start tracking objects

--- src/trackers/__init__.py
+++
@@ -1,1 +0,0 @@
-from src.trackers.tracker import tracker
--- src/trackers/tracker.py
+++ src/trackers/tracker.py
@@ -19,10 +19,47 @@ class tracker:
     def get_object_tracks(self, frames):
         detections = self.detect_frames(frames)
         
+        tracks = {
+            "players":[],
+            "referees":[],
+            "ball":[]
+        }
+
         for frame_num, detection in enumerate(detections):
-            cls_names = detection[0].names  # dictionary form key is 0, 1, 2 and values are player, goalkeeper, ball etc
+            cls_names = detection[0].names  # dictionary form key is 0, 1, 2, 3 and values are ball, goalkeeper, player, referee
             cls_names_inv = {v:k for k, v in cls_names.items()}
 
             # Convert to supervision detection format
             detection_supervision = sv.Detections.from_ultralytics(detection)
-            print(detection_supervision)
+
+            # Convert goalkeeper class to player class
+            for obj_idx, class_id in enumerate(detection_supervision.class_id):
+                if cls_names[class_id] == "goalkeeper":
+                    detection_supervision.class_id[obj_idx] = cls_names_inv["player"]
+
+            # Track Objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+
+            # Appending a dictionary player track id as key and bounding box list as value 
+            #tracks["players"].append({})
+
+            for frame_detection in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+
+                if cls_id == cls_names_inv["player"]:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+
+                if cls_id == cls_names_inv["referee"]:
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+            
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_names_inv["ball"]:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+
+
+            return tracks # dictionary of lists of dictionaries
