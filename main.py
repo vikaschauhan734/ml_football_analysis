6bf8738701e5c24666ab5287d2d54e1a5943be49
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-12-10 22:15:43 +0530

    saved cropped image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.utils.video_utils import read_video, save_video
 from src.trackers.tracker import tracker
+import cv2
 
 def main():
     # Read video
@@ -9,6 +10,19 @@ def main():
     tracke = tracker('models/best_11l.pt')
 
     tracks = tracke.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
+
+    # Save cropped image of a player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+
+        # crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        # save the cropped image
+        cv2.imwrite(f'output_videos/cropped_img.jpg', cropped_image)
+
+        break
 
     # Draw output
     ## Draw object tracks
