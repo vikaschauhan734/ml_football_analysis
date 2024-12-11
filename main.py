bbb08793d4888a8ec5755b9a230e722dc2b86473
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-12-11 19:33:11 +0530

    found tshirt color in notebook

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def main():
 
     tracks = tracke.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
 
-    # Save cropped image of a player
-    for track_id, player in tracks['players'][0].items():
-        bbox = player['bbox']
-        frame = video_frames[0]
+    # # Save cropped image of a player
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
 
-        # crop bbox from frame
-        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+    #     # crop bbox from frame
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
 
-        # save the cropped image
-        cv2.imwrite(f'output_videos/cropped_img.jpg', cropped_image)
+    #     # save the cropped image
+    #     cv2.imwrite(f'output_video/cropped_img.jpg', cropped_image)
 
-        break
+    #     break
 
     # Draw output
     ## Draw object tracks
