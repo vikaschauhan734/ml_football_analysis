ee1199910e24cc3e5eb80a0288aec14beb41abc3
Vikas Chauhan <chauhanvikas734@gmail.com>
2025-01-05 20:59:09 +0530

    speed and distance estimator started

--- src/speed_and_distance_estimator/speed_and_distance_estimator.py
+++ src/speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -2,4 +2,18 @@
 
 class SpeedAndDistanceEstimator():
     def __init__(self):
-        pass
+        self.frame_window = 5 # Calculating speed in between 5 frames.
+        self.frame_rate = 24
+
+    def add_speed_and_distance_to_tracks(self, tracks):
+        for object, object_tracks in tracks.items():
+            if object == "ball" or object == "referees":
+                continue
+            number_of_frames = len(object_tracks)
+            for frame_num in range(0, number_of_frames, self.frame_window):
+                last_frame = min(frame_num+self.frame_window, number_of_frames) # For not go out of bounds
+
+                for track_id, _ in object_tracks[frame_num].items():
+                    if track_id not in object_tracks[last_frame]:
+                        continue # We skip the player which is not in last frame but present in first frame
+                    
