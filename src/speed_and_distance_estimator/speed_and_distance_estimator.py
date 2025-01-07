761e781c35199db61516b7620c474daf04be153d
Vikas Chauhan <chauhanvikas734@gmail.com>
2025-01-07 12:12:19 +0530

    speed and distance estimator continue

--- src/speed_and_distance_estimator/speed_and_distance_estimator.py
+++ src/speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -1,4 +1,4 @@
-
+from src.utils.bbox_utils import measure_distance
 
 class SpeedAndDistanceEstimator():
     def __init__(self):
@@ -6,6 +6,8 @@ class SpeedAndDistanceEstimator():
         self.frame_rate = 24
 
     def add_speed_and_distance_to_tracks(self, tracks):
+        total_distance = {}
+
         for object, object_tracks in tracks.items():
             if object == "ball" or object == "referees":
                 continue
@@ -17,3 +19,26 @@ class SpeedAndDistanceEstimator():
                     if track_id not in object_tracks[last_frame]:
                         continue # We skip the player which is not in last frame but present in first frame
                     
+
+                    start_position = object_tracks[frame_num][track_id]['position_transformed']
+                    end_position = object_tracks[last_frame][track_id]['position_transformed']
+
+                    if start_position is None or end_position is None:
+                        continue
+
+                    distance_covered = measure_distance(start_position, end_position)
+                    time_elapsed = (last_frame-frame_num)/self.frame_rate
+                    speed_meteres_per_second = distance_covered/time_elapsed
+                    speed_km_per_hour = speed_meteres_per_second*3.6
+
+                    if object not in total_distance: # If player not in total_distance then create new key and value
+                        total_distance[object] = {}
+
+                    if track_id not in total_distance[object]: # Initialize diatance as 0 for all track_ids
+                        total_distance[object][track_id] = 0
+
+                    total_distance[object][track_id] += distance_covered
+
+                    for frame_num_batch in range(frame_num, last_frame):
+                        if track_id not in tracks[object][frame_num_batch]:
+                            continue
