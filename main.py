1c4930b0b57a640cfdbc36643327fa509c2044f8
Vikas Chauhan <chauhanvikas734@gmail.com>
2025-01-16 18:00:18 +0530

    significant increase in ball detection

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     #Initialize tracker
-    tracke = tracker('models/best_11l.pt')
+    tracke = tracker('models/best_11l1280.pt')
 
     tracks = tracke.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
 
